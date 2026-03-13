d684827b8d9360ea2f946aa4c8865e3343a7eb5a
Timur Khantimirov <khantimirov2001@gmail.com>
2026-03-13 13:32:09 +0500

    update main.py with 2-opt benchmarks

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from algorithms.genetic import create_population, genetic
 from algorithms.greedy import greedy
 from visualizer import draw_on_ax
 from map_visualizer import get_coordinates, city_from_name, draw_route_on_map
+from algorithms.two_opt import distance, swap_reduces_distance, two_opt
 
 
 #city_names = ["Алматы","Астана","Шымкент","Семей","Караганда"]
@@ -16,8 +17,20 @@ from map_visualizer import get_coordinates, city_from_name, draw_route_on_map
 #greedy_route = greedy(real_cities)
 #draw_route_on_map(real_cities, greedy_route, "route.html")
 
-test_cities = [City(random.randint(0, 100), random.randint(0, 100), f"City_{i}") for i in range(20)]
+test_cities = [City(random.randint(0, 100), random.randint(0, 100), f"City_{i}") for i in range(50)]
 test_route, test_history = genetic(test_cities)
+test_total_route = total_distance(test_route)
+test_two_opt = two_opt(test_route)
+test_total_dist = total_distance(test_two_opt)
+print("До 2-opt:", test_total_route)
+print("После 2-opt:", test_total_dist)
+
+greedy_route = greedy(test_cities)
+print("Greedy:", total_distance(greedy_route))
+greedy_optimized = two_opt(greedy_route)
+print("Greedy + 2-opt:", total_distance(greedy_optimized))
+
+
 
 plt.figure(figsize=(10, 5))
 plt.plot(test_history)
